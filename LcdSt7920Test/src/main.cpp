482b55c58fc3b657cfc60d9cded9f6ce46fc0e66
Kepka Ludovic <shionn@gmail.com>
2017-10-10 22:36:27 +0200

    Test ST7920 en SPI HW

--- LcdSt7920Test/src/main.cpp
+++ LcdSt7920Test/src/main.cpp
@@ -5,19 +5,37 @@
 #include <Wire.h>
 
 
-// RS : CHIP SELECT
-#define RS 10
-// RW : DATA MOSI
-#define RW 11
-// E : CLOCK
-#define E 13
+// CS ou SS ou CHIP SELECT : RS
+#define CS 10
+
+// DATA ou MOSI : R/W
+#define DATA 11
+// CLOCK, E
+#define CLOCK 13
 
 
-U8G2_ST7920_128X64_1_SW_SPI lcd(U8G2_R0, E, RW, RS);
+//U8G2_ST7920_128X64_1_8080 lcd(U8G2_R0, 2, 3, 4, 5, 6, 7, 8, 9, 10, U8X8_PIN_NONE, 12);
+//U8G2_ST7920_128X64_F_SW_SPI lcd(U8G2_R0, /* clock=*/ 13, /* data=*/ 11, /* CS=*/ 10, /* reset=*/ 8);
+U8G2_ST7920_128X64_F_HW_SPI lcd(U8G2_R0, CS);
+
 void setup() {
-    // put your setup code here, to run once:
+  lcd.begin();
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
+  //lcd.clear();
+//  lcd.firstPage();
+//  do {
+//   lcd.setFont(u8g2_font_ncenB12_tr);
+//   lcd.drawStr(5,15,"Hello World !");
+//   lcd.drawBox(5, 17, 118, 2);
+// } while ( lcd.nextPage() );
+
+lcd.clearBuffer();
+lcd.setFont(u8g2_font_ncenB14_tr);
+lcd.drawStr(0,20,"Hello World!");
+   lcd.drawBox(5, 23, 118, 2);
+lcd.sendBuffer();
+
+ delay(1000);
 }
