27dc3e714c14cdca0a0aadf22754fd8cf18c2713
Kepka Ludovic <shionn@gmail.com>
2017-10-10 19:40:54 +0200

    Clean mauvais import

--- LcdSt7920Test/src/main.cpp
+++ LcdSt7920Test/src/main.cpp
@@ -1,5 +1,19 @@
 #include <Arduino.h>
 
+#include <U8g2lib.h>
+#include <SPI.h>
+#include <Wire.h>
+
+
+// RS : CHIP SELECT
+#define RS 10
+// RW : DATA MOSI
+#define RW 11
+// E : CLOCK
+#define E 13
+
+
+U8G2_ST7920_128X64_1_SW_SPI lcd(U8G2_R0, E, RW, RS);
 void setup() {
     // put your setup code here, to run once:
 }
